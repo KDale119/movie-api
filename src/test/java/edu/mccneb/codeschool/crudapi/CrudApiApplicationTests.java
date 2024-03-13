3b17c674698ba9e602d231d8b7e1e1ab18c49a0d
Kayla Dale <kmeyers119@gmail.com>
2024-03-12 19:12:00 -0500

    initial

---
+++ src/test/java/edu/mccneb/codeschool/crudapi/CrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mccneb.codeschool.crudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
